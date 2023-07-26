da4e68601c08001a724975e5023164c8047c51c8
Bhagya Wijenayake <63334319+Bhagyawijenayake@users.noreply.github.com>
2023-07-26 12:59:47 +0530

    angular material setup and login page created

--- apps/dilagro/src/app/app.component.ts
+++ apps/dilagro/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 
 
 @Component({
   standalone: true,
-  imports: [ RouterModule,CommonModule],
+  imports: [ RouterModule,CommonModule,LoginComponent],
   selector: 'seng41293-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
