0f70748f3070ef5988b3aa233e8661839655e601
Bhagya Wijenayake <63334319+Bhagyawijenayake@users.noreply.github.com>
2023-07-26 17:42:14 +0530

    login form start

--- apps/dilagro/src/app/app.component.ts
+++ apps/dilagro/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
+
 
 
 @Component({
   standalone: true,
-  imports: [ RouterModule,CommonModule,LoginComponent],
+  imports: [ RouterModule,CommonModule],
   selector: 'seng41293-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
--- apps/dilagro/src/app/pages/login/login.component.ts
+++ apps/dilagro/src/app/pages/login/login.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'seng41293-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatInputModule, MatButtonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styles: [
-  ]
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  formGroup = new FormGroup({
+    email: new FormControl(),
+    password: new FormControl(),
+  });
 
+  constructor(private router: Router) {}
+  onLogin() {
+    this.router.navigate(['/admin']);
+  }
 }
