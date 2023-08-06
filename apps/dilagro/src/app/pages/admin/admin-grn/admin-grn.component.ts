026e76b529437fcc98d9023a77726efa8533d48b
Bhagya Wijenayake <bhagyasudaraka98@gmail.com>
2023-08-06 09:13:35 +0530

    admin-grn component created

---
+++ apps/dilagro/src/app/pages/admin/admin-grn/admin-grn.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'seng41293-admin-grn',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './admin-grn.component.html',
+  styleUrls: ['./admin-grn.component.scss']
+})
+export class AdminGrnComponent {
+
+}
