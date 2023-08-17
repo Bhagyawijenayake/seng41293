19f36f19c00b461961c4b859672bf79e1a55f3c4
Bhagya Wijenayake <bhagyasudaraka98@gmail.com>
2023-08-17 17:03:28 +0530

    source map explore added and toolbar commented

--- apps/dilagro/src/app/app.component.ts
+++ apps/dilagro/src/app/app.component.ts
@@ -7,26 +7,28 @@ import { Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { AppState } from './state/app/app.state';
 
-
-
 @Component({
   standalone: true,
-  imports: [ RouterModule,CommonModule,MatToolbarModule,MatProgressBarModule],
+  imports: [
+    RouterModule,
+    CommonModule,
+    //MatToolbarModule,
+    //MatProgressBarModule
+  ],
   selector: 'seng41293-root',
-  templateUrl:'./app.component.html',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-email$:Observable<string>;
-loading$:Observable<boolean>;
+  email$: Observable<string>;
+  loading$: Observable<boolean>;
 
-
- constructor (private store:Store) {
-  this.email$ = this.store.select(AppState.email)
-  this.loading$ = this.store.select(AppState.loading)
-  // this.email$.subscribe((email)=>{
-  //   console.log(email)
-  // }
-  // )
- }
+  constructor(private store: Store) {
+    this.email$ = this.store.select(AppState.email);
+    this.loading$ = this.store.select(AppState.loading);
+    // this.email$.subscribe((email)=>{
+    //   console.log(email)
+    // }
+    // )
+  }
 }
