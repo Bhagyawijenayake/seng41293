9ed1dbde69e10fa39ab57f81015611be38f29c36
Bhagya Wijenayake <bhagyasudaraka98@gmail.com>
2023-08-23 21:09:15 +0530

    mat card create and http client

--- apps/dilagro/src/app/app.config.ts
+++ apps/dilagro/src/app/app.config.ts
@@ -15,6 +15,8 @@ import { provideServiceWorker } from '@angular/service-worker';
 import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { AngularFireModule } from '@angular/fire/compat';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { HttpClientModule } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -26,17 +28,17 @@ export const appConfig: ApplicationConfig = {
       })
     ),
     importProvidersFrom(
-
-        AngularFireModule.initializeApp({
-          apiKey: 'AIzaSyDuQlZdWFjP3cA5hUrlGjn7UNAqovAdM18',
-          authDomain: 'dilagro-8d8b9.firebaseapp.com',
-          projectId: 'dilagro-8d8b9',
-          storageBucket: 'dilagro-8d8b9.appspot.com',
-          messagingSenderId: '1032247810734',
-          appId: '1:1032247810734:web:e91084ee557e1698342b3f',
-        })
-
+      AngularFireModule.initializeApp({
+        apiKey: 'AIzaSyDuQlZdWFjP3cA5hUrlGjn7UNAqovAdM18',
+        authDomain: 'dilagro-8d8b9.firebaseapp.com',
+        projectId: 'dilagro-8d8b9',
+        storageBucket: 'dilagro-8d8b9.appspot.com',
+        messagingSenderId: '1032247810734',
+        appId: '1:1032247810734:web:e91084ee557e1698342b3f',
+      })
     ),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(HttpClientModule),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
--- apps/dilagro/src/app/pages/admin/admin-grn/admin-grn.component.ts
+++ apps/dilagro/src/app/pages/admin/admin-grn/admin-grn.component.ts
@@ -1,25 +1,39 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppService } from '../../../services/app/app.service';
-import {  MatButtonModule } from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { Store } from '@ngxs/store';
 import { ShowLoading } from '../../../state/app/app.actions';
+import { HttpClient } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+import {MatCardModule} from '@angular/material/card';
+
+
+interface User {
+
+  name: string;
+
+  email: string;
+}
 
 @Component({
   selector: 'seng41293-admin-grn',
   standalone: true,
-  imports: [CommonModule, MatButtonModule],
+  imports: [CommonModule, MatButtonModule,MatCardModule],
   templateUrl: './admin-grn.component.html',
   styleUrls: ['./admin-grn.component.scss'],
 })
 export class AdminGrnComponent {
   @Input({ required: true }) label!: string;
 
-  @Output() update = new EventEmitter<string>();
+  users$: Observable<User[]>;
 
-  constructor(private store: Store) {}
-
-  toggle() {
-    this.store.dispatch(new ShowLoading(true));
+  constructor(private httpClient: HttpClient) {
+   this.users$= this.httpClient
+      .get<User[]>('https://jsonplaceholder.typicode.com/users')
+      
+      
   }
 }
+
+//https://jsonplaceholder.typicode.com/photos
